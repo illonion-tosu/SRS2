5299daaa0911660fd3b4c804292d4999b1d3343c
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-09-05 15:45:44 +1000

    Add now playing stats

--- main/index.js
+++ main/index.js
@@ -1,6 +1,16 @@
 // Variables
 const EZ_MULTIPLIER = 1.75
 
+// Get Beatmaps
+let allBeatmaps
+async function getBeatmaps() {
+    const response = await axios.get("../_data/beatmaps.json")
+    allBeatmaps = response.data.beatmaps
+}
+getBeatmaps()
+// Find Beatmaps
+const findBeatmaps = beatmapId => allBeatmaps.find(beatmap => Number(beatmap.beatmap_id) === Number(beatmapId))
+
 // Team Name Variables
 const teamNameLeftEl = document.getElementById("team-name-left")
 const teamNameRightEl = document.getElementById("team-name-right")
@@ -10,7 +20,14 @@ let currentTeamNameLeft, currentTeamNameRight
 const nowPlayingBannerEl = document.getElementById("now-playing-banner")
 const nowPlayingTitleDifficultyEl = document.getElementById("now-playing-title-difficulty")
 const nowPlayingArtistEl = document.getElementById("now-playing-artist")
-let currentBeatmapId, currentBeatmapChecksum
+let currentBeatmapId, currentBeatmapChecksum, currentMappoolBeatmap
+// Now Playing Stats
+const nowPlayingSrEl = document.getElementById("now-playing-sr")
+const nowPlayingCsEl = document.getElementById("now-playing-cs")
+const nowPlayingBpmEl = document.getElementById("now-playing-bpm")
+const nowPlayingArEl = document.getElementById("now-playing-ar")
+const nowPlayingHpEl = document.getElementById("now-playing-hp")
+const nowPlayingLenEl = document.getElementById("now-playing-len")
 
 // Spclet
 const socket = createTosuWsSocket()
@@ -28,12 +45,60 @@ socket.onmessage = event => {
         teamNameRightEl.textContent = currentTeamNameRight
     }
 
-    if (currentBeatmapId !== data.beatmap.id || currentBeatmapChecksum !== data.beatmap.checksum) {
+    if ((currentBeatmapId !== data.beatmap.id || currentBeatmapChecksum !== data.beatmap.checksum) && allBeatmaps) {
         currentBeatmapId = data.beatmap.id
         currentBeatmapChecksum = data.beatmap.checksum
 
+        // Metadata
         nowPlayingBannerEl.style.backgroundImage = `url("https://assets.ppy.sh/beatmaps/${data.beatmap.set}/covers/cover.jpg")`
         nowPlayingTitleDifficultyEl.textContent = `${data.beatmap.title} [${data.beatmap.version}]`
         nowPlayingArtistEl.textContent = data.beatmap.artist
+
+        // Find beatmap
+        currentMappoolBeatmap = findBeatmaps(currentBeatmapId)
+        if (currentMappoolBeatmap) {
+            const allStats = getStats(
+                Math.round(Number(currentMappoolBeatmap.difficultyrating) * 100) / 100,
+                Number(currentMappoolBeatmap.diff_approach),
+                Number(currentMappoolBeatmap.diff_size),
+                Number(currentMappoolBeatmap.diff_drain),
+                Number(currentMappoolBeatmap.bpm),
+                Number(currentMappoolBeatmap.total_length)
+            )
+
+            nowPlayingSrEl.textContent = `${allStats.sr}*`
+            nowPlayingCsEl.textContent = allStats.cs
+            nowPlayingBpmEl.textContent = allStats.bpm
+            nowPlayingArEl.textContent = allStats.ar
+            nowPlayingHpEl.textContent = allStats.hp
+            nowPlayingLenEl.textContent = setLengthDisplay(allStats.len)
+        }
     }
+
+    if (!currentMappoolBeatmap) {
+        nowPlayingSrEl.textContent = `${data.beatmap.stats.stars.total}*`
+        nowPlayingCsEl.textContent = data.beatmap.stats.cs.converted
+        nowPlayingBpmEl.textContent = data.beatmap.stats.bpm.common
+        nowPlayingArEl.textContent = data.beatmap.stats.ar.converted
+        nowPlayingHpEl.textContent = data.beatmap.stats.hp.converted
+        nowPlayingLenEl.textContent = setLengthDisplay(Math.round((data.beatmap.time.lastObject - data.beatmap.time.firstObject) / 1000))
+    }
+}
+
+// Get Stats
+function getStats(sr, ar, cs, hp, bpm, len, mod) {
+    if (currentMappoolBeatmap.mod.includes("HR")) {
+        cs = Math.min(Math.round(cs * 1.3 * 10) / 10, 10)
+        ar = Math.min(Math.round(ar * 1.4 * 10) / 10, 10)
+        hp = Math.min(Math.round(hp * 1.4 * 10) / 10, 10)
+    }
+    if (currentMappoolBeatmap.mod.includes("DT")) {
+        if (ar > 5) ar = Math.round((((1200 - (( 1200 - (ar - 5) * 150) * 2 / 3)) / 150) + 5) * 10) / 10
+        else ar = Math.round((1800 - ((1800 - ar * 120) * 2 / 3)) / 120 * 10) / 10
+        if (hp > 5) hp = Math.round((((1200 - (( 1200 - (hp - 5) * 150) * 2 / 3)) / 150) + 5) * 10) / 10
+        else hp = Math.round((1800 - ((1800 - hp * 120) * 2 / 3)) / 120 * 10) / 10
+        bpm = Math.round(bpm * 1.5)
+        len = Math.round(len / 1.5)
+    }
+    return {sr: sr, ar: ar, cs: cs, hp: hp, bpm: bpm, len: len, mod: mod}
 }
