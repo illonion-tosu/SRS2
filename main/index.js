e9c74928337a676f95c5a5abf65c7d13ebb6f871
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-09-05 16:30:42 +1000

    Chat display

--- main/index.js
+++ main/index.js
@@ -48,6 +48,10 @@ const animation = {
 // Scorebar
 const scorebarStarEl = document.getElementById("scorebar-star")
 
+// Chat display container
+const messagesContainerEl = document.getElementById("messages-container")
+let chatLen = 0
+
 // Spclet
 const socket = createTosuWsSocket()
 socket.onmessage = event => {
@@ -104,7 +108,6 @@ socket.onmessage = event => {
     }
 
     // Score visibility
-    console.log(scoreVisibility)
     if (scoreVisibility !== data.tourney.scoreVisible) {
         scoreVisibility = data.tourney.scoreVisible
         if (scoreVisibility) {
@@ -157,6 +160,38 @@ socket.onmessage = event => {
         } else if (currentRightScore === currentLeftScore) {
             scorebarStarEl.style.left = `960px`
             scorebarStarEl.setAttribute("src", `static/stars/middle-star-full.png`)
+        }
+    }
+
+    // Chat display
+    if (!scoreVisibility) {
+        // This is also mostly taken from Victim Crasher: https://github.com/VictimCrasher/static/tree/master/WaveTournament
+        if (chatLen !== data.tourney.chat.length) {
+            (chatLen === 0 || chatLen > data.tourney.chat.length) ? (messagesContainerEl.innerHTML = "", chatLen = 0) : null
+            const fragment = document.createDocumentFragment()
+
+            for (let i = chatLen; i < data.tourney.chat.length; i++) {
+                // Chat message container
+                const chatMessageContainer = document.createElement("div")
+                chatMessageContainer.classList.add("message-container")  
+
+                // Name
+                const chatMessageName = document.createElement("div")
+                chatMessageName.classList.add("message-name", data.tourney.chat[i].team)
+                chatMessageName.textContent = `${data.tourney.chat[i].name}:`
+
+                // Message
+                const chatMessageContent = document.createElement("div")
+                chatMessageContent.classList.add("message-content")
+                chatMessageContent.innerText = data.tourney.chat[i].message
+
+                chatMessageContainer.append(chatMessageName, chatMessageContent)
+                fragment.append(chatMessageContainer)
+            }
+
+            messagesContainerEl.append(fragment)
+            chatLen = data.tourney.chat.length
+            messagesContainerEl.scrollTop = messagesContainerEl.scrollHeight
         }
     }
 }
