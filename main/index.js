81d698dd3a8014b7fa55e7346b865834cb986c80
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-09-05 15:09:35 +1000

    Add metadata to the now playing area

--- main/index.js
+++ main/index.js
@@ -1,7 +1,16 @@
+// Variables
+const EZ_MULTIPLIER = 1.75
+
 // Team Name Variables
 const teamNameLeftEl = document.getElementById("team-name-left")
 const teamNameRightEl = document.getElementById("team-name-right")
 let currentTeamNameLeft, currentTeamNameRight
+
+// Now Playing
+const nowPlayingBannerEl = document.getElementById("now-playing-banner")
+const nowPlayingTitleDifficultyEl = document.getElementById("now-playing-title-difficulty")
+const nowPlayingArtistEl = document.getElementById("now-playing-artist")
+let currentBeatmapId, currentBeatmapChecksum
 
 // Spclet
 const socket = createTosuWsSocket()
@@ -17,5 +26,14 @@ socket.onmessage = event => {
     if (currentTeamNameRight !== data.tourney.team.right) {
         currentTeamNameRight = data.tourney.team.right
         teamNameRightEl.textContent = currentTeamNameRight
+    }
+
+    if (currentBeatmapId !== data.beatmap.id || currentBeatmapChecksum !== data.beatmap.checksum) {
+        currentBeatmapId = data.beatmap.id
+        currentBeatmapChecksum = data.beatmap.checksum
+
+        nowPlayingBannerEl.style.backgroundImage = `url("https://assets.ppy.sh/beatmaps/${data.beatmap.set}/covers/cover.jpg")`
+        nowPlayingTitleDifficultyEl.textContent = `${data.beatmap.title} [${data.beatmap.version}]`
+        nowPlayingArtistEl.textContent = data.beatmap.artist
     }
 }
